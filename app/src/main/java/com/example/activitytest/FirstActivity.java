6a717defeaadb6a46f8f687e285af062ded71a20
yuanfajun <1014176753@qq.com>
2017-12-15 14:02:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -0,0 +1,9 @@
+package com.example.activitytest;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017/12/13/013.
+ */
+public class FirstActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/activitytest/FourthActivity.java
@@ -0,0 +1,26 @@
+package com.example.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FourthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fourth_layout);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FourthActivity.this, "You clicked Button 2",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }
+}
